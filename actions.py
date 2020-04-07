0b46406cd2b9b4b94ab9c90d53b2c4ada808bb70
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-04-07 11:20:04 +0200

    initial commit

---
+++ actions.py
@@ -0,0 +1,81 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+from rasa_sdk.forms import FormAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Tracker
+from typing import Any, Dict, List, Text, Union
+from rasa_sdk import Action
+from rasa_sdk.events import UserUtteranceReverted
+
+class SalesForm(FormAction):
+    """Collects sales information and adds it to the spreadsheet"""
+
+    def name(self):
+        return "sales_form"
+
+    @staticmethod
+    def required_slots(tracker):
+        return [
+            "job_function",
+            "use_case",
+            "budget",
+            "person_name",
+            "company",
+            "business_email",
+            ]     
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict]:
+
+        dispatcher.utter_message("Thanks for getting in touch, we’ll contact you soon")
+        return []    
+
+    # TypeError: Parameters to generic types must be types. Got slice(<class 'str'>, typing.Union[typing.Dict, typing.List[typing.Dict]], None).
+    #def slot_mappings(self) -> Dict[Text: Union[Dict, List[Dict]]]:
+    def slot_mappings(self):
+        # type: () -> Dict[Text: Union[Dict, List[Dict]]]
+        """A dictionary to map required slots to
+        - an extracted entity
+        - intent: value pairs
+        - a whole message
+        or a list of them, where a first match will be picked"""
+        return {"use_case": self.from_text(intent="inform")}                   
+
+class ActionGreetUser(Action):
+    """Revertible mapped action for utter_greet"""
+
+    def name(self):
+        return "action_greet"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_template("utter_greet", tracker)
+        return [UserUtteranceReverted()]
---
+++ data/stories.md
@@ -0,0 +1,40 @@
+## thank
+* thank
+  - utter_noworries
+
+## goodbye
+* bye
+  - utter_bye
+  
+## Some question from FAQ
+* faq
+    - respond_faq  
+
+## sales form
+* contact_sales
+    - sales_form                   <!--Run the sales_form action-->
+    - form{"name": "sales_form"}   <!--Activate the form-->
+    - form{"name": null}           <!--Deactivate the form-->    
+
+## explain email    <!-- https://github.com/RasaHQ/rasa/issues/2180 -->   
+* contact_sales
+    - sales_form
+    - form{"name": "sales_form"}
+    - slot{"requested_slot": "business_email"}
+
+* explain
+    - utter_explain_why_email
+    - sales_form
+    - form{"name": null}
+
+## explain budget
+* contact_sales
+    - sales_form
+    - form{"name": "sales_form"}
+    - slot{"requested_slot": "budget"}
+    - form{"name": null}
+
+* explain
+    - utter_explain_why_budget
+    - sales_form
+    - form{"name": null}
